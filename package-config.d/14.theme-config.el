f201cf582d1cc93f5726f9c1138738abf26a8dff
K. C. Juntunen <k.c.juntunen@yorkwwt.com>
2025-12-05 07:56:43 -0500

    Set fonts after theming.

--- package-config.d/14.theme-config.el
+++ package-config.d/14.theme-config.el
@@ -6,15 +6,6 @@
 								"92831daa963b36a6436ba2759f54161846e7af0958251cb5d033ddccc04d6bd3" ;; leuven
 								default))
 
-(set-face-attribute 'cursor nil :background "OrangeRed1")
-(set-face-attribute 'fixed-pitch-serif nil :font "Victor Mono")
-(set-face-attribute 'fixed-pitch nil :font "Victor Mono")
-(set-face-attribute 'variable-pitch nil :font "Cambria")
-(set-face-attribute 'default nil :font "Victor Mono")
-(set-face-attribute 'org-table nil :height 0.9)
-(set-face-attribute 'org-drawer nil :height 0.7)
-(set-face-attribute 'org-meta-line nil :height 0.7)
-
 (defun kc/set-theme ()
 	"Load theme depending on dark/light mode."
 	(interactive)
@@ -26,7 +17,17 @@
 													 "AppsUseLightTheme")))))
 				 (if dark-mode
 						 (load-theme 'leuven-dark t)
-					 (load-theme 'leuven t))))
+					 (load-theme 'leuven t))
+(set-face-attribute 'cursor nil :background "OrangeRed1")
+(set-face-attribute 'fixed-pitch-serif nil :font "Victor Mono")
+(set-face-attribute 'fixed-pitch nil :font "Victor Mono")
+(set-face-attribute 'variable-pitch nil :font "Cambria")
+(set-face-attribute 'default nil :font "Victor Mono")
+(set-face-attribute 'org-table nil :height 0.9)
+(set-face-attribute 'org-drawer nil :height 0.7)
+(set-face-attribute 'org-meta-line nil :height 0.7)
+(set-face-attribute 'org-block-begin-line nil :height 0.7)
+(set-face-attribute 'org-block-end-line nil :height 0.7)))
 
 (kc/set-theme)
 (global-set-key (kbd "C-c t") 'kc/set-theme)
