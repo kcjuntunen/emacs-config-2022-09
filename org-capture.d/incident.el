86219f40377e28c8427e3c9e1c4aa00b88dbf5c1
K. C. Juntunen <k.c.juntunen@yorkwwt.com>
2025-12-16 10:29:38 -0500

    Add incident thing to capture list.

---
+++ org-capture.d/incident.el
@@ -0,0 +1,15 @@
+(defun kc/incident-wrapper ()
+	"Call `york-get-incident-data' interactively."
+	(with-temp-buffer
+		(let ((standard-output (current-buffer)))
+			(call-interactively #'york-get-incident-data))
+		(substring (buffer-string) 2)))
+
+(add-to-list 'org-capture-templates
+						 '("e" "Incident"
+							 entry
+							 (file kc/interruption-file)
+							 "* %?%(kc/incident-wrapper)"
+							 :clock-in t
+							 :clock-resume t))
+
