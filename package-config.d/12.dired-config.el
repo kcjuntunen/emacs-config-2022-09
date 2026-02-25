c206eb5b65820ca0b098236408a91ce94cb46cfb
K. C. Juntunen <k.c.juntunen@yorkwwt.com>
2026-02-25 08:08:55 -0500

    Remove that message, etc.

--- package-config.d/12.dired-config.el
+++ package-config.d/12.dired-config.el
@@ -5,15 +5,14 @@
 	(autoload 'dired-async-mode "dired-async.el" nil t)
 	(dired-async-mode 1))
 
-(add-hook #'dired-mode-hook
+(add-hook 'dired-mode-hook
 					(lambda ()
 						(dired-hide-details-mode)
-						(hl-line-mode 1)
+						;; (hl-line-mode 1)
 						(text-scale-adjust -1)
 						(if (or (string-equal (buffer-name) "workarea")
 										(string-equal (buffer-name) "fastrack"))
-								(auto-revert-mode 1)
-							(message "Buffer name is %s" (buffer-name)))))
+								(auto-revert-mode 1))))
 
 (if (not (string-equal system-type "darwin"))
 		(setq dired-listing-switches "-alhGF --dired --group-directories-first")
